3ac25a84a6090b49526aa72dcb831d771a82bd8b
Louis <leon.louis14@gmail.com>
2019-09-03 16:32:02 -0700

    Added some defs

--- mergeSort.py
+++ mergeSort.py
@@ -1,3 +1,30 @@
 import os
 
-print("HelloWorld")
+
+def main():
+    with open("input.txt", "r") as file:
+        line = [int(x) for x in file.readline().split()]
+    print(mergeSort(line))
+
+
+def mergeSort(numList):
+    left = []
+    right = []
+
+    if len(numList) <= 1:
+        return numList
+
+    for x in range(len(numList)):
+        if numList[x] < (len(numList)/2):
+            left.append(numList[x])
+        else:
+            right.append(numList[x])
+    print(left);
+    print(right);
+
+def merge():
+    pass
+
+
+if __name__ == '__main__':
+    main()
